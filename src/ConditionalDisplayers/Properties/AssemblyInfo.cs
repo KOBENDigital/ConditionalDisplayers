5900e7b670b0c111b72b5e7953a659c1ef8f71b8
Mario Lopez <mario@koben.com.au>
2019-01-02 09:32:15 +1100

    changed project to Our.Umbraco

--- src/ConditionalDisplayers/Properties/AssemblyInfo.cs
+++ src/ConditionalDisplayers/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("965cdf4c-043f-4aed-ab61-1d61b9ff6489")]
+[assembly: Guid("ca7f24cd-805c-405e-9ed5-adf24536eb6b")]
 
 // Version information for an assembly consists of the following four values:
 //

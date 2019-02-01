08938b401ba7ea30fe0b8892cd8898ffbb38909d
Mario Lopez <mario@koben.com.au>
2019-02-01 11:30:10 +1100

    added checkbox value converter

---
+++ src/ConditionalDisplayers/ConditionalCheckboxConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.ConditionalDisplayers
+{
+    [PropertyValueType(typeof(bool))]
+    class ConditionalCheckboxConverter : IPropertyValueConverter
+    {
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object data, bool preview)
+        {
+            var attemptConvertInt = data.TryConvertTo<bool>();
+            if (attemptConvertInt.Success)
+            {
+                return attemptConvertInt.Result;
+            }
+
+            return null;
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return (bool)source;
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("Our.Umbraco.CdCheckbox");
+        }
+    }
+}

abacdefb1b0b36d8494d029ff34befc368961b97
Mario Lopez <mario@koben.com.au>
2019-02-02 08:04:04 +1100

    fixes #4 . Value turn to true on saving.

--- src/ConditionalDisplayers/App_Plugins/ConditionalDisplayers/propertyeditors/checkbox/checkbox.controller.js
+++ src/ConditionalDisplayers/App_Plugins/ConditionalDisplayers/propertyeditors/checkbox/checkbox.controller.js
@@ -30,36 +30,49 @@ function cdCheckboxController($scope) {
         return h;
     }
 
-    $scope.runDisplayLogic = function () {
-        if (!$scope.model.value) $scope.model.value = false;
+    $scope.$watch("renderModel.value", function (newVal) {
+        $scope.model.value = newVal === true ? "1" : "0";
 
-        if ($scope.model.value) {
+        $scope.runDisplayLogic();
+    });
+
+    $scope.clicked = function () {
+        $scope.renderModel.value = !$scope.renderModel.value;
+    }
+
+    $scope.runDisplayLogic = function () {
+        //init visible fields
+        if ($scope.renderModel.value) {
             displayProps($scope.model.config.showIfChecked, $scope.model.config.showIfUnchecked);
         } else {
             displayProps($scope.model.config.showIfUnchecked, $scope.model.config.showIfChecked);
         }
-    };
-
-    $scope.clicked = function () {
-        $scope.model.value = !$scope.model.value;
-        $scope.runDisplayLogic();
     }
 
-
-
-    function setupViewModel() {  
+    function setupViewModel() {
+        $scope.renderModel = {
+            value: false
+        };
 
         if ($scope.model.config && $scope.model.config.default && $scope.model.config.default.toString() === "1" && $scope.model && !$scope.model.value) {
-            $scope.model.value = true;
+            $scope.renderModel.value = true;
         }
 
         if ($scope.model && $scope.model.value && ($scope.model.value.toString() === "1" || angular.lowercase($scope.model.value) === "true")) {
-            $scope.model.value = true;
+            $scope.renderModel.value = true;
         }
 
-        //init visible fields
         $scope.runDisplayLogic();
     }
+
+
+    //here we declare a special method which will be called whenever the value has changed from the server
+    //this is instead of doing a watch on the model.value = faster
+    $scope.model.onValueChanged = function (newVal, oldVal) {
+        //update the display val again if it has changed from the server
+        setupViewModel();
+    };
+
 
     setupViewModel();
 

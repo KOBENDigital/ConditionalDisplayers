605c02047a66b70cca42ba8510a4dbfe8f0f7693
mario <lopezadeh@gmail.com>
2019-11-02 14:41:50 +1100

    removed some rubbish from the project

--- src/ConditionalCheckboxConverter.cs
+++ src/ConditionalCheckboxConverter.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Umbraco.Core;
-using Umbraco.Core.Models.PublishedContent;
-using Umbraco.Core.PropertyEditors;
+﻿using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Core.PropertyEditors.ValueConverters;
 
 namespace Our.Umbraco.ConditionalDisplayers
@@ -15,7 +8,7 @@ namespace Our.Umbraco.ConditionalDisplayers
         public override bool IsConverter(IPublishedPropertyType propertyType)
         {
             return propertyType.EditorAlias == "Our.Umbraco.CdCheckbox";
-        }   
+        }
     }
 
 
